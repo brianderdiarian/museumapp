8936126560fe453e8b1a4f85b15b2e274a5bf8b3
brianderdiarian <brianderdiarian@gmail.com>
2017-01-01 14:29:43 -0800

    updated local config exception handling

--- museum_project/settings.py
+++ museum_project/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 from urllib.parse import urlparse
-from config import LOCAL_SECRET_KEY, LOCAL_DEBUG
+
+try:
+    from config import LOCAL_SECRET_KEY, LOCAL_DEBUG
+except:
+    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

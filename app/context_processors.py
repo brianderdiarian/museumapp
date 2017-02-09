3d79ffecccd67ff7509b73df41db9986aef5c0a9
brianderdiarian <brianderdiarian@gmail.com>
2017-02-09 13:31:59 -0800

    Distinct artist names in dropdown

--- app/context_processors.py
+++ app/context_processors.py
@@ -6,7 +6,7 @@ def movement_processor(request):
 	movements = Movement.objects.filter(artist__artwork__display__end_date__gte=today).order_by('movement').distinct()
 	#movements = sorted(set(movements))
 
-	artists = Display.objects.exclude(end_date__lt=today).exclude(start_date__gt=today).order_by('artwork__artist__artist_sans_accents').distinct()
+	artists = Display.objects.exclude(end_date__lt=today).exclude(start_date__gt=today).order_by('artwork__artist__artist_sans_accents').distinct('artwork__artist__artist_sans_accents')
 
 	collections = Collection.objects.filter(display__start_date__lte=today).filter(display__end_date__gte=today).annotate(num_displays=Count('display')).order_by('-num_displays').distinct()
 

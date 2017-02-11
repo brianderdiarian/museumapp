780dc724cd2b4314e46926f4c23a5c599f27b71c
brianderdiarian <brianderdiarian@gmail.com>
2017-02-10 21:20:46 -0800

    Added error handling

--- museum_bot/museum_bot/spiders/whitney.py
+++ museum_bot/museum_bot/spiders/whitney.py
@@ -39,110 +39,113 @@ class MetSpider(Spider):
 
         results = jsonresponse['group_objects']['results']
 
+        
         for result in results:
+            try:
+                accession_number = "WHIT" + result['accession_number']
 
-            accession_number = "WHIT" + result['accession_number']
+                if Display.objects.filter(artwork__accession_number=accession_number).filter(end_date=yesterday).exists():
+                    Display.objects.filter(artwork__accession_number=accession_number).filter(end_date=yesterday).update(end_date=today)
 
-            if Display.objects.filter(artwork__accession_number=accession_number).filter(end_date=yesterday).exists():
-                Display.objects.filter(artwork__accession_number=accession_number).filter(end_date=yesterday).update(end_date=today)
+                elif Display.objects.filter(artwork__accession_number=accession_number).exists():
 
-            elif Display.objects.filter(artwork__accession_number=accession_number).exists():
+                    collection = Collection.objects.get(collection_name__contains="Whitney")
 
-                collection = Collection.objects.get(collection_name__contains="Whitney")
+                    artwork = Artwork.objects.get(accession_number=accession_number)
 
-                artwork = Artwork.objects.get(accession_number=accession_number)
+                    start_date = datetime.date.today().isoformat()
 
-                start_date = datetime.date.today().isoformat()
+                    end_date = datetime.date.today().isoformat()
 
-                end_date = datetime.date.today().isoformat()
+                    yield DisplayItem(
+                        collection = collection,
+                        artwork = artwork,
+                        start_date = start_date,
+                        end_date = end_date,
+                    )
+                
+                else:
+                    if result['artist_name'] == " ":
+                        artist = "Unknown Artist"
+                        artist_sans_accents = "Unknown Artist"
+                    elif result['artist_name'] == "Unknown":
+                        artist = "Unknown Artist"
+                        artist_sans_accents = "Unknown Artist"
+                    else:    
+                        artist = strip_parenthesis(result['artist_name'].replace("&#39;","'")).strip()
+                        artist_sans_accents = remove_accents(artist)
+                        artist_sans_accents = artist_sans_accents.replace(',','')
+                 
+                    title = result['title'].replace("&#39;","'")
 
-                yield DisplayItem(
-                    collection = collection,
-                    artwork = artwork,
-                    start_date = start_date,
-                    end_date = end_date,
-                )
-            
-            else:
-                if result['artist_name'] == " ":
-                    artist = "Unknown Artist"
-                    artist_sans_accents = "Unknown Artist"
-                elif result['artist_name'] == "Unknown":
-                    artist = "Unknown Artist"
-                    artist_sans_accents = "Unknown Artist"
-                else:    
-                    artist = strip_parenthesis(result['artist_name'].replace("&#39;","'")).strip()
-                    artist_sans_accents = remove_accents(artist)
-                    artist_sans_accents = artist_sans_accents.replace(',','')
-             
-                title = result['title'].replace("&#39;","'")
-
-                title_sans_accents = remove_accents(title)
-             
-                try:
-                    date = result['display_date']
-                except:
-                    date = "Date Unknown"
-             
-                #medium = result['medium']
-             
-                description = "None"
-             
-                dimensions = "None"
-
-                collection = Collection.objects.get(collection_name__contains="Whitney")
-             
-                try:
-                    if result['primary_image_id'] == 0:
-                        imageurl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
-                    else:
-                        imageurl = "http://collectionimages.whitney.org/standard/" + str(result['primary_image_id']) + "/listing.jpg"
-                except:
-                    imageurl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
-
-                pageurl = "http://collection.whitney.org/object/" + str(result['object_id'])
-             
-                timestamp = datetime.date.today().isoformat()
-
-                start_date = datetime.date.today().isoformat()
-
-                end_date = datetime.date.today().isoformat()
-
-                try:
-                    artist = NameVariant.objects.get(name=artist_sans_accents).artist
-                except:
+                    title_sans_accents = remove_accents(title)
+                 
                     try:
-                        artist = Artist.objects.get(artist_sans_accents=artist_sans_accents)
+                        date = result['display_date']
                     except:
-                        artist = artist_sans_accents
-                        yield ArtistItem(
-                            artist_sans_accents=artist_sans_accents,
-                        )
-                        time.sleep(2)
-                        artist = Artist.objects.get(artist_sans_accents=artist_sans_accents)
+                        date = "Date Unknown"
+                 
+                    #medium = result['medium']
+                 
+                    description = "None"
+                 
+                    dimensions = "None"
 
-                yield ArtworkItem(
-                    title=title,
-                    title_sans_accents=title_sans_accents,
-                    date=date,
-                    #medium=medium,
-                    description=description,
-                    dimensions=dimensions,
-                    collection=collection,
-                    imageurl=imageurl,
-                    pageurl=pageurl,
-                    accession_number=accession_number,
-                    timestamp=timestamp,
-                    artist=artist,
-                )
+                    collection = Collection.objects.get(collection_name__contains="Whitney")
+                 
+                    try:
+                        if result['primary_image_id'] == 0:
+                            imageurl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
+                        else:
+                            imageurl = "http://collectionimages.whitney.org/standard/" + str(result['primary_image_id']) + "/listing.jpg"
+                    except:
+                        imageurl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
 
-                from app.models import Artwork
+                    pageurl = "http://collection.whitney.org/object/" + str(result['object_id'])
+                 
+                    timestamp = datetime.date.today().isoformat()
 
-                artwork = Artwork.objects.get(accession_number=accession_number)
+                    start_date = datetime.date.today().isoformat()
 
-                yield DisplayItem(
-                    collection = collection,
-                    artwork = artwork,
-                    start_date = start_date,
-                    end_date = end_date,
-                )
+                    end_date = datetime.date.today().isoformat()
+
+                    try:
+                        artist = NameVariant.objects.get(name=artist_sans_accents).artist
+                    except:
+                        try:
+                            artist = Artist.objects.get(artist_sans_accents=artist_sans_accents)
+                        except:
+                            artist = artist_sans_accents
+                            yield ArtistItem(
+                                artist_sans_accents=artist_sans_accents,
+                            )
+                            time.sleep(2)
+                            artist = Artist.objects.get(artist_sans_accents=artist_sans_accents)
+
+                    yield ArtworkItem(
+                        title=title,
+                        title_sans_accents=title_sans_accents,
+                        date=date,
+                        #medium=medium,
+                        description=description,
+                        dimensions=dimensions,
+                        collection=collection,
+                        imageurl=imageurl,
+                        pageurl=pageurl,
+                        accession_number=accession_number,
+                        timestamp=timestamp,
+                        artist=artist,
+                    )
+
+                    from app.models import Artwork
+
+                    artwork = Artwork.objects.get(accession_number=accession_number)
+
+                    yield DisplayItem(
+                        collection = collection,
+                        artwork = artwork,
+                        start_date = start_date,
+                        end_date = end_date,
+                    )
+                except:
+                    continue

efbfc139a49c066c3cbedc21f736af6d55c5c6f2
brianderdiarian <brianderdiarian@gmail.com>
2017-04-04 20:07:11 -0700

    revert

--- app/context_processors.py
+++ app/context_processors.py
@@ -3,14 +3,16 @@ from .tools import current, today#, mlist, artistlist, artist_by_movement
 from django.db.models import Count
 
 def movement_processor(request):
-	movements = Movement.objects.filter(artist__artwork__display__end_date__gte=today).order_by('movement').values_list('movement', flat=True).distinct()
+	movements = Movement.objects.filter(artist__artwork__display__end_date__gte=today).order_by('movement').distinct()
+	#movements = sorted(set(movements))
 
-	#artists = Artist.objects.filter(artwork__display__end_date__gte=today).filter(artwork__display__start_date__lte=today).order_by('artist_sans_accents').distinct('artist_sans_accents').select_related()
-	artists = Artist.objects.filter(artwork__display__end_date__gte=today).filter(artwork__display__start_date__lte=today).order_by('artist_sans_accents').values_list('artist_sans_accents', flat=True).distinct()
+	artists = Artist.objects.filter(artwork__display__end_date__gte=today).filter(artwork__display__start_date__lte=today).order_by('artist_sans_accents').distinct('artist_sans_accents').select_related()
 
-	#collections = Collection.objects.filter(display__start_date__lte=today).filter(display__end_date__gte=today).annotate(num_displays=Count('display')).order_by('-num_displays').distinct()
-	collections = Collection.objects.filter(display__start_date__lte=today).filter(display__end_date__gte=today).annotate(num_displays=Count('display')).order_by('-num_displays').values_list('collection_name', flat=True).distinct()
+	collections = Collection.objects.filter(display__start_date__lte=today).filter(display__end_date__gte=today).annotate(num_displays=Count('display')).order_by('-num_displays').distinct()
+
 
 	return {'movements': movements,
 			'artists': artists,
-			'collections': collections}
+			'collections': collections,
+			# 'artist_by_movement': artist_by_movement
+			}

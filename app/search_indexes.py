6fa2d1b0218cac808a03d8770cb39f891aae805f
brianderdiarian <brianderdiarian@gmail.com>
2017-04-05 18:47:08 -0700

    Added filtering

--- app/search_indexes.py
+++ app/search_indexes.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from haystack import indexes
 from app.models import Display, Artwork
+from app.tools import today
 
 class DisplayIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
@@ -11,3 +12,7 @@ class DisplayIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_model(self):
         return Display
+
+    def index_queryset(self, using=None):
+    # Used when the entire index for model is updated
+    return self.get_model().objects.filter(start_date__lte=today).filter(end_date__gte=today)
